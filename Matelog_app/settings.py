9e7112c785e0c3168186e8ca9b584f466ce77505
Karla Fernanda <karlafernandariveramartinez@gmail.com>
2026-05-07 21:02:56 -0600

    fix login api

--- Matelog_app/settings.py
+++ Matelog_app/settings.py
@@ -34,7 +34,11 @@ ALLOWED_HOSTS = [
     "localhost",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://matelog-app.onrender.com",
+]
 
+APPEND_SLASH = False
 
 # Application definition
 
@@ -67,7 +71,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
