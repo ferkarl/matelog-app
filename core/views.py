099af750df730d843d7604e6dd86263e1801f940
Karla Fernanda <karlafernandariveramartinez@gmail.com>
2026-05-07 21:31:18 -0600

    debug login

--- core/views.py
+++ core/views.py
@@ -98,7 +98,17 @@ class EjerciciosViewSet(viewsets.ModelViewSet):
 # =========================
 # LOGIN
 # =========================
+@api_view(['POST'])
+def login_view(request):
 
+    print("ENTRO AL LOGIN VIEW")
+
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    print(username)
+    print(password)
+    
 @api_view(['POST'])
 def login_view(request):
 
