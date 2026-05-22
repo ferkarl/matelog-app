1b4a2f6d2978e8944ce534a0a11bf744e6c52628
Karla Fernanda <karlafernandariveramartinez@gmail.com>
2026-05-21 21:17:44 -0600

    Fix REST framework config (AllowAny correction)

--- Matelog_app/settings.py
+++ Matelog_app/settings.py
@@ -65,8 +65,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PARSER_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
